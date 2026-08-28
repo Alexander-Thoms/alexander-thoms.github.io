a4a571900ca368749ca421e1a6b1d0d4ca61ce93
Alexander-Thoms <alex.g.thoms@gmail.com>
2026-08-28 14:12:38 -0400

    Revert flush alignment; enlarge music video to 320px

--- app/page.jsx
+++ app/page.jsx
@@ -139,10 +139,10 @@ export default function Page() {
             </section>
 
             <section className="section page">
-              <div className="music-nowplaying">
+              <div className="section-title-wrap">
                 <h2 className="shine-on-hover">now playing</h2>
-                <div id="music-video" className="music-video"></div>
               </div>
+              <div id="music-video" className="music-video"></div>
             </section>
           </div>
         </main>
