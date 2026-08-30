c6801967ef37611a028870e9cded71c102aac217
Alexander-Thoms <alex.g.thoms@gmail.com>
2026-08-29 22:52:44 -0400

    Add icon.ico favicon link (fixes missing tab icon)

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         />
+        <link rel="icon" href="/assets/icon.ico" type="image/x-icon" />
       </head>
       <body>{children}</body>
     </html>
